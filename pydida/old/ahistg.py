d681e4c76a23173b328e977c0b1223c7d901ec34
Tomi Silander <tsilande@celebes.ad.europe.naverlabs.com>
2021-07-16 16:47:57 +0200

    cleaned a bit

--- pydida/old/ahistg.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-def ahistg(vals, tfields):
-    fixes = dict([(v,i) for i,v in enumerate(vals)])
-    hg = [0]*len(fixes)
-    for f in tfields:
-        if f in fixes:
-            hg[fixes[f]] += 1
-    return hg
